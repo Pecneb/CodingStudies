f07f19476c5d7b8e73343ba53837a3bd8108e7ca
pecneb <pecneb@pop-os.localdomain>
2021-04-29 00:11:18 +0200

    Optimalizalas->sizeof_file

--- 13_het/sizeof_file/main.cpp
+++ 13_het/sizeof_file/main.cpp
@@ -7,17 +7,11 @@ int main(int argc, char* argv[]) {
     if(argc != 2) {
         cout << "Hasznalat: " << argv[0] << " <filename>" << endl;
     } else {
-        ifstream file(argv[1], ios_base::binary);
+        ifstream file(argv[1]);
         if(file.is_open()) {
-            int filesize;
-            while(file.get(), !file.eof()) filesize++;
-            if(filesize < 1000) {
-                cout << "Size: " << (filesize) << endl;
-            } else if(filesize > 999 && filesize < 1000000) {
-                cout << "Size: " << (filesize/1000) << "K" << endl;
-            } else {
-                cout << "Size: " << (filesize/1000000) << "M" << endl;
-            }
+            file.seekg(0, ios_base::end);
+            streampos vege = file.tellg();
+            cout << "Size: " << vege << " bajt";
             file.close();
         } else {
             cerr << "Problema a fajl megnyitasakor!";
